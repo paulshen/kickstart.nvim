fb7a5ff41ccddcdd84b4538c2e1021157efd4611
Paul Shen <paulshen0@gmail.com>
2024-09-29 00:14:41 -0700

    gh for lsp hover

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -20,6 +20,7 @@ return {
       { "E", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "gh", mode = { "n" }, function() vim.lsp.buf.hover() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
       { "<c-j>", mode = { "n", "x", "o" }, function() require("flash").jump({
         search = { mode = "search", forward = true, wrap = false, max_length = false },
